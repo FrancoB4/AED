e3162f6ebbc8a4f05d094fce3cdccbdd4d84d011
FrancoB4 <francobonfigliovazquez@gmail.com>
2022-05-28 01:18:02 -0300

    Some improvements un TP2

--- Trabajos Practicos/TP2/TP2.py
+++ Trabajos Practicos/TP2/TP2.py
@@ -1,44 +1,65 @@
 import random
 
+# Constantes necesarias para generar cada carta
 VALORES = 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11
 PALOS = 'Diamante', 'Corazones', 'Pica', 'Trébol'
-# Esta tupla nos servirá para luego asignar un nombre a las cartas con valor 10.
 FIGURAS = 10, 'J', 'Q', 'K'
-
-texto_figura = ''
-texto_resultado = ''
-texto_mismo_palo = ''
 
 
 def separador(n, salto):
-    """Una función que se utiliza para separar diferentes segmentos de la interfaz."""
+    """Separa diferentes segmentos de la interfaz"""
     if salto:
         print('\n')
     print('-' * n)
 
 
 def dar_carta(mano, valores, palos, figuras, puntos):
+    """Toma los datos actuales de la mano del jugador o del crupier y los retorna actualizados habiéndoseles sumado una
+    carta.
+    """
     nueva_mano = mano
-    es_as = False
     carta = random.choice(valores), random.choice(palos)
+
+    # Si la carta vale 10, se le asigna una figura
     if carta[0] == 10:
         carta = carta[0], carta[1], random.choice(figuras)
+    # Si la carta tiene valor 11, se indica que es un As
     elif carta[0] == 11:
+
+        # Si el puntaje se excediera de 21, el As adopta automáticamente un valor de 1
         if puntos + 11 > 21:
             carta = 1, carta[1], 'As'
         else:
             carta = carta[0], carta[1], 'As'
-        es_as = True
     else:
         carta = carta[0], carta[1], str(carta[0])
 
+    # Se actualizan el puntaje y la mano
     puntos_actualizados = puntos + carta[0]
     nueva_mano += carta,
-    return nueva_mano, puntos_actualizados, es_as
+
+    # Si el puntaje supera los 21 puntos, busca entre las cartas de la mano si alguna es un As y cambiamos su valor
+    if puntos_actualizados > 21:
+        mano_control_as = ()
+        puntos_actualizados = 0
+
+        for carta in nueva_mano:
+            nueva_carta = ()
+
+            if carta[0] == 11:
+                nueva_carta += (1, carta[1], carta[2])
+                mano_control_as += nueva_carta,
+                puntos_actualizados += nueva_carta[0]
+            else:
+                mano_control_as += carta,
+                puntos_actualizados += carta[0]
+
+        return mano_control_as, puntos_actualizados
+    return nueva_mano, puntos_actualizados
 
 
 def mostrar_opciones():
-    """Esta función muestra el menu de opciones y retorna la opcion escogida por el usuario"""
+    """Muestra el menu de opciones y retorna la opcion escogida por el usuario."""
     print('Bienvenido al juego de BlackJack 2.0!')
     print('¿Que desea hacer?')
     print('[1] Apostar')
@@ -56,33 +77,33 @@ def mostrar_opciones():
 
 
 def mostrar_cartas(cartas, jugador):
+    """Muestra la mano del jugador o del crupier."""
     print(f'Mano del {jugador}:')
     for carta in cartas:
         print(f'{carta[2]} de {carta[1]} con un valor de {carta[0]}')
 
 
 def menu(nombre_jugador, monto):
-    """Definimos una función que representa el cuerpo del programa, aquí se recibe la opcion del usuario y se ejecuta
-    la alternativa correspondiente a esa opcion.
+    """Cuerpo del programa, aquí se recibe la opcion del usuario y se ejecuta la alternativa correspondiente a esa
+    opcion.
     """
 
-    # Esta variable acumulará el monto disponible para jugar
+    # Variables que vamos a necesitar para las salidas
     acumulado = monto
-    # Declaramos las variables que vamos a mostrar al salir del juego
     victorias = derrotas = racha_mas_larga = racha_actual = manos_bj_natural = cant_apuestas = total_apuestas = \
         mayor_perdida = 0
-    monto_maximo = acumulado
-    bj_natural = True
+    monto_max = acumulado
 
+    # Se muestran las ociones del menu
     opcion = mostrar_opciones()
 
+    # Se ejecutará siempre mientras el usuario no ingrese la opcion de salir del juego
     while True:
-        if opcion == 1:
-            # Aquí se ejecuta la opción 1 (apuesta) del programa.
-            apuesta = 0
 
-            # Controlamos que la apuesta cumpla con la condición requerida (debe ser un número positivo y el acumulado)
-            # No debe sobrepasar 100000.
+        # Opción 1 (apuesta)
+        if opcion == 1:
+            apuesta = 0
+            # Se controla que la apuesta cumpla con la condición requerida (0 < apuesta < 100000)
             while apuesta + acumulado > 100_000 or apuesta <= 0:
                 apuesta = int(input('Ingrese la nueva apuesta: '))
 
@@ -92,17 +113,19 @@ def menu(nombre_jugador, monto):
                 elif apuesta <= 0:
                     print('La apuesta ingresada no es valida, por favor ingrese una apuesta mayor que cero.')
 
-            # Actualizamos el acumulado y lo imprimimos en consola.
+            # Se actualiza y muestra el acumulado.
             acumulado += apuesta
             print(f'Su nuevo monto acumulado es de: {acumulado}')
+
+        # Opción 2 del programa, es el desarrollo del juego en sí.
         elif opcion == 2:
-            # Aquí se ejecuta la opción 2 del programa, es el desarrollo del juego en sí.
+            # Variables necesarias para las salidas.
             cant_apuestas += 1
-            # Inicialización de las variables de los puntos de cada jugador.
             puntos_jugador, puntos_crupier, monto_a_jugar = 0, 0, 0
             monto_inicial = acumulado
+            bj_natural = bj_natural_crupier = False
 
-            # Le pedimos al jugador que realice su apuesta
+            # Se define el monto apostado de esta ronda
             while monto_a_jugar <= 0 or monto_a_jugar > acumulado or monto_a_jugar % 5 != 0:
                 monto_a_jugar = int(input('Su apuesta esta ronda (0 para salir al menú): '))
                 separador(20, False)
@@ -112,43 +135,45 @@ def menu(nombre_jugador, monto):
                     print('Ingrese un múltiplo de 5.')
                 elif monto_a_jugar < 0:
                     print('ingrese un valor valido (>= 0).')
+                # Incluimos la opcion de volver al menu
                 elif monto_a_jugar == 0:
                     break
 
-            # En esta condición verificamos que el jugador no haya ingresado la opcion de volver al menú.
+            # Si el jugador no desea volver al menu, inicia la mano
             if monto_a_jugar != 0:
                 total_apuestas += monto_a_jugar
                 mano_jugador = ()
                 mano_crupier = ()
-                # Le damos dos cartas al jugador y actualizamos su puntaje
-                mano_jugador, puntos_jugador, hay_as_jugador = dar_carta(mano_jugador, VALORES, PALOS, FIGURAS, 0)
-                mano_jugador, puntos_jugador, hay_as_jugador = dar_carta(mano_jugador, VALORES, PALOS, FIGURAS,
-                                                                         puntos_jugador)
+
+                # El jugador recibe dos cartas y se actualiza su puntaje
+                for _ in range(2):
+                    mano_jugador, puntos_jugador = dar_carta(mano_jugador, VALORES, PALOS, FIGURAS, puntos_jugador)
+
+                # Se define si existió Nlack Jack natural
+                if puntos_jugador == 21:
+                    bj_natural = True
+                    manos_bj_natural += 1
 
                 # Le damos una carta al crupier
-                mano_crupier, puntos_crupier, hay_as_crupier = dar_carta(mano_crupier, VALORES, PALOS, FIGURAS, 0)
+                mano_crupier, puntos_crupier = dar_carta(mano_crupier, VALORES, PALOS, FIGURAS, puntos_jugador)
 
-                # Mostramos las cartas que se han repartido:
+                # Se muestran las manos del jugador y del crupier:
                 mostrar_cartas(mano_jugador, 'jugador')
                 print(f'Tus puntos son: {puntos_jugador}')
-
                 separador(20, False)
-
                 mostrar_cartas(mano_crupier, 'crupier')
 
+                # El jugador indica si desea recibir otra carta:
                 otra_carta = input('Desea recibir otra carta? [s/n]: ')
                 separador(20, True)
                 while otra_carta == 's' or otra_carta == 'S':
-                    mano_jugador, puntos_jugador, hay_as_jugador = dar_carta(mano_jugador, VALORES, PALOS, FIGURAS,
-                                                                             puntos_jugador)
+                    mano_jugador, puntos_jugador = dar_carta(mano_jugador, VALORES, PALOS, FIGURAS, puntos_jugador)
+
+                    # Si el jugador se pasa o consigue 21 puntos, no puede pedir mas cartas
                     if puntos_jugador > 21:
-                        if hay_as_jugador:
-                            print('Hay as')
-                            break
-                        else:
-                            print('Te has pasado de 21 puntos.')
-                            puntos_jugador = 0
-                            break
+                        print('Te has pasado de 21 puntos.')
+                        puntos_jugador = 0
+                        break
                     elif puntos_jugador == 21:
                         print('Tienes 21 puntos!')
                         break
@@ -157,42 +182,51 @@ def menu(nombre_jugador, monto):
                         otra_carta = input(f'Tu puntaje actual es de {puntos_jugador} deseas otra carta? [s/n]: ')
                     separador(20, True)
 
+                # El crupier recibe cartas mientras su puntaje sea menor o igual a 16
                 while puntos_crupier <= 16:
-                    mano_crupier, puntos_crupier, hay_as_crupier = dar_carta(mano_crupier, VALORES, PALOS, FIGURAS,
-                                                                             puntos_crupier)
+                    mano_crupier, puntos_crupier = dar_carta(mano_crupier, VALORES, PALOS, FIGURAS,
+                                                             puntos_crupier)
 
-                    if puntos_crupier > 21:
-                        if hay_as_crupier:
-                            pass
-                        else:
-                            puntos_crupier = 0
-                            break
+                    # En su segunda carta, se verifica si hay Black Jack natural
+                    if puntos_jugador == 21 and len(mano_jugador) == 2:
+                        bj_natural_crupier = True
+                    elif puntos_crupier > 21:
+                        puntos_crupier = 0
+                        break
 
-                if puntos_jugador > puntos_crupier:
-                    # Cargamos un mensaje para mostrar que gano el jugador
+                # Se determina el resultado de la mano, teniendo en cuenta los puntajes y BJ naturales
+                if puntos_jugador > puntos_crupier or (puntos_jugador == puntos_crupier and bj_natural
+                                                       and not bj_natural_crupier):
+                    # Se carga el mensaje para mostrar que gano el jugador
                     mensaje_ganador = f'Ha ganado el jugador con {puntos_jugador} puntos contra {puntos_crupier} ' \
                                       f'puntos del crupier.'
 
-                    # Actualizamos las variables del jugador
+                    # Se actualizan las variables del jugador
                     acumulado += monto_a_jugar
                     victorias += 1
                     racha_actual += 1
                     if racha_actual > racha_mas_larga:
                         racha_mas_larga = racha_actual
-                elif puntos_jugador == puntos_crupier and puntos_jugador != 0:
+                elif puntos_jugador == puntos_crupier and puntos_jugador != 0 and bj_natural == bj_natural_crupier:
                     # Cargamos un mensaje para mostrar que hubo empate
                     mensaje_ganador = f'Ha habido un empate! Ambos tienen {puntos_jugador} puntos.'
                 else:
-                    # Si el crupier tiene mas puntos que el jugador o ambos se pasan de 21, gana el crupier y se
-                    # Crea el mensaje para mostrar
+                    # Se carga el mensaje para mostrar que gano el crupier (también válido si ambos se pasan)
                     mensaje_ganador = f'Ha ganado el crupier con {puntos_crupier} puntos contra {puntos_jugador} ' \
                                       f'puntos del jugador'
+
+                    # Se actualizan las variables del jugador
                     acumulado -= monto_a_jugar
                     derrotas += 1
+                    if racha_actual > racha_mas_larga:
+                        racha_mas_larga = racha_actual
                     racha_actual = 0
                     if monto_a_jugar > mayor_perdida:
                         mayor_perdida = monto_a_jugar
 
+                # Se actualiza el monto máximo si corresponde
+                if acumulado > monto_max:
+                    monto_max = acumulado
                 print(f'El jugador inició la mano con un monto acumulado de {monto_inicial}')
                 print(f'Realizo una apuesta por {monto_a_jugar}')
 
@@ -204,26 +238,32 @@ def menu(nombre_jugador, monto):
 
                 print(mensaje_ganador)
                 print(f'El monto acumulado del jugador es de {acumulado}')
-                input('\nPresione enter para continuar')
+                input('\nPresione enter para continuar: ')
+
+        # Aquí se ejecuta la opción 3 del programa, se muestran los resultados y se termina con la ejecución.
         elif opcion == 3:
-            # Aquí se ejecuta la opción 3 del programa, se muestran los resultados y se termina con la ejecución.
+
+            # Control de division por cero
             if victorias or derrotas != 0:
                 porcentaje_victorias = round(victorias / (victorias + derrotas) * 100, 2)
             else:
                 porcentaje_victorias = 0
+
+            # Se calcula el monto promedio de las apuestas
             prom_apuestas = total_apuestas / cant_apuestas
 
+            # Se muestran los datos solicitados
             print(f'El porcentaje de victorias del jugador fué de: {porcentaje_victorias}%')
             print(f'La mayor racha de victorias fue: {racha_mas_larga}')
             print(f'La cantidad de manso con BlackJack natural fue: {manos_bj_natural}')
+            print(f'El mayor monto alcanzado por el jugador fue: {monto_max}')
             print(f'El promedio de los montos apostados fue: {prom_apuestas}')
             print(f'La mayor perdida del jugador fue de: {mayor_perdida}')
 
-            # Salimos del bucle para terminar con la ejecución del programa.
+            # Salida del bucle y fin del juego.
             break
 
-        # Si la opción ingresada fue 1 o 2, el bucle no termina y se vuelve a mostrar el menú y cargar la opcion del
-        # usuario.
+        # Si la opcion fue 1 o 2: el bucle no termina y se vuelve a mostrar el menú y cargar la opcion del usuario.
         separador(30, True)
         opcion = mostrar_opciones()
 
@@ -231,9 +271,7 @@ def menu(nombre_jugador, monto):
 
 
 def run():
-    """Creamos una function que contenga la raíz del script para evitar problemas de definición al ejecutar las
-    demás funciones
-    """
+    """Raíz del script para evitar problemas de definición al ejecutar las demás funciones."""
     nombre = input('Ingrese su nombre: ')
 
     monto = 0
@@ -243,6 +281,7 @@ def run():
             print('El valor ingresado es invalido, por favor ingrese un numero entre [1:100000]')
 
     separador(20, True)
+    # Llamada a la función principal del juego
     menu(nombre, monto)
 
 
